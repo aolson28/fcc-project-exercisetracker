a98201f028799e9f88416ad7044d5ff01cd96dee
Aaron Olson <aaronmichael.olson@gmail.com>
2024-05-28 02:47:20 +0000

    update

---
+++ mongodb/mongo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+    
+const getUsers = (req, res) => {
+    return;
+};
+
+const addUser = (req, res) => {    
+    return;
+};
+
+const addExercise = (req, res) => {
+    return;
+};
+
+const getUserLog = (req, res) => {
+    return;
+};
+
+module.exports = {getUsers, addUser, addExercise, getUserLog};
