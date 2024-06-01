29e32023deb7de6a5060e70c851026549019b614
Aaron Olson <aaronmichael.olson@gmail.com>
2024-06-01 19:59:57 +0000

    update to module format

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const { connectMongoDB } = require('./mongodb/connectMongoDB.js')
 const { Schema, model } = mongoose;
 require('dotenv').config()
 const { url } = require('inspector');
+const mongoRouter = require('./mongodb/mongo.js');
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.json());
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
@@ -17,87 +19,16 @@ app.use(express.urlencoded({
   extended: true
 }));
 
+app.use('/api', mongoRouter);
+
+
 // connect to MongoDB using Mongoose in /mongodb/connectMongoDB.js
 // put connection URL in .env as MONGO_URL
 connectMongoDB();
 
-const userSchema = new Schema(
-  {
-      username: String
-  }
-);
-
-const exerciseSchema = new Schema(
-  {
-      username: String,
-      description: String,
-      duration: Number,
-      date: Date,
-      userId: String
-  }
-);
-
-const User = model('User', userSchema);
-const Exercise = model('Exercise', exerciseSchema);
-
-app.post('/api/users', (req, res) => {
-  let newUser = new User({
-    username: req.body.username
-  });    
-  newUser.save().then((data) => {
-      res.json({username: data.username, _id: data.id});
-  }).catch((err) => {
-      if (err) {
-          console.error(err);
-      }
-  });
-});
-
-app.post('/api/users/:_id/exercises', (req, res) => {
-  User.findById(req.params._id).then((data) => {
-    let activityDate = req.body.date ? new Date(req.body.date).toDateString() : new Date().toDateString();
-    let newExercise = new Exercise({username: data.username, description: req.body.description, duration: Number(req.body.duration), date: activityDate, userId: data.id});
-    newExercise.save().then((data) => {return;}).catch((err) => {if (err) {console.error(err);}});
-    res.json({username: data.username, description: req.body.description, duration: Number(req.body.duration), date: activityDate, _id: data.id});
-  }).catch((err) => {
-    if(err) {
-      console.error(err)
-    }});
-});
-
-app.get('/api/users', (req, res) => {
-  User.find({}).then((data) => {
-    res.send(data.map((item) => {return {username: item.username, _id: item.id};}));
-}).catch((err) => {
-    if (err) {
-        console.error(err);
-    }
-});
-});
-
-app.get('/api/users/:_id/logs?:from?/:to?/:limit?', (req, res) => {
-    User.findById(req.params._id).then((data) => {
-      let logQuery = Exercise.find({userId: data.id});
-      if (req.query.from) {
-        logQuery = logQuery.where('date').gte(req.query.from);
-      } else {}
-      if (req.query.to) {
-        logQuery = logQuery.where('date').lte(req.query.to);
-      } else {}
-      if (req.query.limit) {
-        logQuery = logQuery.limit(req.query.limit);
-      } else {}
-      logQuery.select('description duration date').then((logData) => {
-        res.json({
-          username: data.username,
-          count: logData.length,
-          _id: data.id,
-          log: [...logData.map((item) => {return {description: item.description, duration: item.duration, date: item.date.toDateString()}})]
-        })
-      }).catch((err) => {if (err) {console.error(err);}})
-    }).catch((err) => {if (err) {console.error(err);}});
-});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+module.exports = app;
--- mongodb/mongo.js
+++ mongodb/mongo.js
@@ -1,65 +1,85 @@
-/* const mongoose = require('mongoose');
+var express = require('express');
+var router = express.Router();
+const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
-const { url } = require('inspector');
+
+
 const userSchema = new Schema(
     {
-        username: String,
-        count: Number,
-        log: [{description: String, duration: Number, date: Date}]
+        username: String
     }
-);
-
+  );
+  
 const exerciseSchema = new Schema(
-    {
-        username: String,
-        description: String,
-        duration: Number,
-        date: Date
-    }
+{
+    username: String,
+    description: String,
+    duration: Number,
+    date: Date,
+    userId: String
+}
 );
 
 const User = model('User', userSchema);
 const Exercise = model('Exercise', exerciseSchema);
 
-const getUsers = () => {
-    User.find({}).then((data) => {
-        console.log(data);
-        return data;
-    }).catch((err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
-};
+router.post('/users', (req, res) => {
+console.log(req);
+let newUser = new User({
+    username: req.body.username
+});    
+newUser.save().then((data) => {
+    res.json({username: data.username, _id: data.id});
+}).catch((err) => {
+    if (err) {
+        console.error(err);
+    }
+});
+});
 
-const addUser = (username) => {
-    let newUser = new User({
-        username: username,
-        count: 0,
-        log: []
-    });    
-    let res = newUser.save().then((data) => {
-        return {username: data.username, _id: data.id};
-    }).catch((err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
-    console.log(res);
-    return res;
-};
+router.post('/users/:_id/exercises', (req, res) => {
+User.findById(req.params._id).then((data) => {
+    let activityDate = req.body.date ? new Date(req.body.date).toDateString() : new Date().toDateString();
+    let newExercise = new Exercise({username: data.username, description: req.body.description, duration: Number(req.body.duration), date: activityDate, userId: data.id});
+    newExercise.save().then((data) => {return;}).catch((err) => {if (err) {console.error(err);}});
+    res.json({username: data.username, description: req.body.description, duration: Number(req.body.duration), date: activityDate, _id: data.id});
+}).catch((err) => {
+    if(err) {
+    console.error(err)
+    }});
+});
 
-const addExercise = (req, res) => {
-    return;
-};
+router.get('/users', (req, res) => {
+User.find({}).then((data) => {
+    res.send(data.map((item) => {return {username: item.username, _id: item.id};}));
+}).catch((err) => {
+    if (err) {
+        console.error(err);
+    }
+});
+});
 
-const getUserLog = (req, res) => {
-    let from;
-    let to;
-    let limit;
-    return;
-};
+router.get('/users/:_id/logs?:from?/:to?/:limit?', (req, res) => {
+    User.findById(req.params._id).then((data) => {
+    let logQuery = Exercise.find({userId: data.id});
+    if (req.query.from) {
+        logQuery = logQuery.where('date').gte(req.query.from);
+    } else {}
+    if (req.query.to) {
+        logQuery = logQuery.where('date').lte(req.query.to);
+    } else {}
+    if (req.query.limit) {
+        logQuery = logQuery.limit(req.query.limit);
+    } else {}
+    logQuery.select('description duration date').then((logData) => {
+        res.json({
+        username: data.username,
+        count: logData.length,
+        _id: data.id,
+        log: [...logData.map((item) => {return {description: item.description, duration: item.duration, date: item.date.toDateString()}})]
+        })
+    }).catch((err) => {if (err) {console.error(err);}})
+    }).catch((err) => {if (err) {console.error(err);}});
+});
 
-module.exports = {getUsers, addUser, addExercise, getUserLog};
-
-*/
+module.exports = router;
